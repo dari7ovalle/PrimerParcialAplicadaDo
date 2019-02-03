19a397588f68c7727af8f3a071d38de47ffba40e
dari7ovalle <35406686+dari7ovalle@users.noreply.github.com>
2019-02-03 09:37:40 -0400

    subir Primeros paso

---
+++ DAL/Contexto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+
+namespace DAL
+{
+    public class Contexto : DbContext
+
+    {
+        //public DbSet<Categorias> Categorias { get; set; }
+        //public DbSet<TiposEgresos> TiposEgresos { get; set; }
+        //public DbSet<Presupuestos> Presupuestos { get; set; }
+        //public DbSet<Articulos> Articulo { get; set; }
+        //public DbSet<Cotizaciones> Cotizaciones { get; set; }
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+}
